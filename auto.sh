f2f9397583ccb494fbe40dc609c25fde276ceb29
ArcticFoox <jongsin16@naver.com>
2024-01-16 11:07:17 +0900

    shell upload

---
+++ auto.sh
@@ -0,0 +1,25 @@
+echo 'start'
+
+filePath=`git status -u -s | head -n 1`
+
+echo ${filePath:2}
+
+commitMag=`cat ${filePath:2} | head -1`
+
+echo $commitMag
+
+problemId=`echo ${commitMag:2} | cut -d ' ' -f1`
+problemKind=`echo ${commitMag:2} | cut -d ' ' -f2`
+problemURL=`echo ${commitMag:2} | cut -d ' ' -f3`
+problemCommit=`echo -e $problemKind'\n'$problemURL`
+
+echo $problemId
+echo $problemKind
+echo $problemURL
+echo $problemCommit
+
+git add ${filePath:2}
+git commit -m "$problemId" -m "${problemCommit:3}"
+git push
+
+echo "end"
