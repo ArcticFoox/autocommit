b70b34c471520a2d136bf46294a3b79ec4ee3b23
ArcticFoox <jongsin16@naver.com>
2025-04-15 23:21:23 +0900

    2742
    basic
    https://www.acmicpc.net/problem/2742

---
+++ tupN.cpp
@@ -0,0 +1,16 @@
+//2742 basic
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    cin >> n;
+
+    cout << n << '\n';
+    while(--n){
+        cout << n << '\n';
+    }
+    return 0;
+}
