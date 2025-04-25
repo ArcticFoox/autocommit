81156db9e4c45925ee47b80fdd00913377e4e0e9
ArcticFoox <jongsin16@naver.com>
2025-04-25 23:01:39 +0900

    25314
    basic
    https://www.acmicpc.net/problem/25314

---
+++ codeisphysical.cpp
@@ -0,0 +1,16 @@
+//25314 basic
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n / 4; i++){
+        cout << "long ";
+    }
+    
+    cout << "int";
+    return 0;
+}
